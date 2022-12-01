a9d5ebfce047480a7d604d85b7c456f942acb402
roy gebrayel <roy.gebrayel10@gmail.com>
2022-12-01 20:48:55 +0200

    creating the first table

---
+++ DDL.sql
@@ -0,0 +1,11 @@
+-- Active: 1669894427276@@127.0.0.1@3306@carrace
+
+CREATE TABLE RacingCar (
+    RC INTEGER AUTO_INCREMENT NOT NULL,
+    power INTEGER,
+    color VARCHAR (255),
+    PRIMARY KEY (RC)
+    
+);
+
+
--- testing.sql
+++
@@ -1,5 +0,0 @@
--- Active: 1669894427276@@127.0.0.1@3306@carrace
-CREATE Table tett( 
-    dt INTEGER,
-    sup VARCHAR (3)
-);
