213a57bba3807fa36b641dfee719a7beffcd1f0e
roy gebrayel <roy.gebrayel10@gmail.com>
2022-12-04 14:42:35 +0200

    trying to connect to database using php

---
+++ example.sql
@@ -0,0 +1,8 @@
+-- Active: 1670154204282@@127.0.0.1@3306@carrace
+CREATE Table registration(
+    fullName VARCHAR(255),
+    PRIMARY KEY (fullName)
+);
+
+INSERT INTO registration (fullName)
+VALUES ('roy');
