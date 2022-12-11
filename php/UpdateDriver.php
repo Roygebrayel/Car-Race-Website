b73694d95c0a66d25d6b7f873d1e3e97b2ce2ae1
roy gebrayel <roy.gebrayel10@gmail.com>
2022-12-11 13:47:49 +0200

    done with the select sql statement from starbucks city center w yalla wle

--- php/UpdateDriver.php
+++ php/UpdateDriver.php
@@ -1,88 +1,27 @@
+<?php
 
-// defining variables of Racing Driver
+$servername = "localhost";
+$username = "root";
+$password = "NewPassword";
+$dbname = "carrace";
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "NewPassword";
-// $dbname = "carrace";
-// $fullName = $_POST['fullName'];
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
 
-// $oldemail = $_POST['email'];
-// $newemail = $_POST['email'];
+$sql = "SELECT RD, fullName, rk FROM racing_driver";
+$result = mysqli_query($conn, $sql);
 
+if (mysqli_num_rows($result) > 0) {
+// output data of each row
+while($row = mysqli_fetch_assoc($result)) {
+echo "id: " . $row["RD"]. " - Name: " . $row["fullName"]. " " . $row["rk"]. "<br>";
+}
+} else {
+echo "0 results";
+}
 
-
-
-// // Create connection
-
-// $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// // Check connection
-
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-
-// //writing the sql quereus
-
-
-
-
-// if (isset($_GET['submit'])) {
-
-
-// $email = $_GET['demail'];
-
-// }
-// // $query = mysql_query("select * from employee", $connection);
-// while ($row = mysql_fetch_array($query)) {
-// echo "<b><a href='updatephp.php?update={$row['employee_id']}'>{$row['employee_name']}</a></b>";
-// echo "<br />";
-// }
-// $query = "update racing_driver set
-//  employee_email='$email'
-//  where employee_id='$id'";
-// $updateEmail = "UPDATE racing_driver SET email = '$newemail'
-// WHERE $oldemail = 'email' ;
-// ";
-
-// //check quereus
-
-
-// if (mysqli_query($conn, $updateEmail)) {
-//     echo "New record in Racing Driver table created successfully";
-// } else {
-//     echo "Error: " . $updateEmail . "<br>" . mysqli_error($conn);
-// }
-
-
-// //closing the connection
-
-
-// mysqli_close($conn);
-
-
-//  -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // <!-- 
-// // $RD = "SELECT RD FROM table Racing_Driver";
-// // $updateName = "UPDATE racing_driver SET fullName = '$fullName'
-// // WHERE RD=$RD;"; -->
-
-
+mysqli_close($conn);
